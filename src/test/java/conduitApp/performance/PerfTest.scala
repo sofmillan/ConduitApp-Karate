911e102837b407e25deb8c922749fc188c458298
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-01-22 15:25:10 -0500

    Simulation setup

--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -12,9 +12,16 @@ class PerfTest extends Simulation {
   ) 
   val createArticle = scenario("Create and delete article").exec(karateFeature("classpath:conduitApp/performance/CreateArticle.feature"))
   
-
+  //Inject a user is a session starting our scenario
   setUp(
-    createArticle.inject(atOnceUsers(2)
+    createArticle.inject(
+      atOnceUsers(1),
+      nothingFor(4 seconds),
+      constantUsersPerSec(1) during (10 seconds),
+      constantUsersPerSec(2) during (10 seconds),
+      rampUsersPerSec(2) to 10 during (20 seconds),
+      nothingFor(5 seconds),
+      constantUsersPerSec(1) during (5 seconds)
     ).protocols(protocol)  
     )
 
