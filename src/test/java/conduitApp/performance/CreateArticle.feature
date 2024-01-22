22ab412241fff328d1895e291d1b20ffd5840c7f
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-01-22 17:26:42 -0500

    Feeder from file

--- src/test/java/conduitApp/performance/CreateArticle.feature
+++ src/test/java/conduitApp/performance/CreateArticle.feature
@@ -4,12 +4,12 @@ Background: Define URL
     Given url apiUrl
     * def articleRequestBody = read('classpath:conduitApp/json/newArticleRequest.json')
     * def dataGenerator = Java.type('helpers.DataGenerator')
-    * set articleRequestBody.article.title = dataGenerator.getRandomArticleJson().title
+    * set articleRequestBody.article.title = __gatling.Title
     * set articleRequestBody.article.body = dataGenerator.getRandomArticleJson().body
-    * set articleRequestBody.article.description = dataGenerator.getRandomArticleJson().description
+    * set articleRequestBody.article.description = __gatling.Description
 
     * def sleep = function(ms){java.lang.Thread.sleep(ms)}
-    * def pause = karate.get('__gastling.pause', sleep)
+    * def pause = karate.get('__gatling.pause', sleep)
 
 Scenario: Create and Delete an article
 
--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -10,18 +10,20 @@ class PerfTest extends Simulation {
     "/api/articles/{articleId}" -> Nil  //multiple requests of the same endpoint will be reported together
 
   ) 
-  val createArticle = scenario("Create and delete article").exec(karateFeature("classpath:conduitApp/performance/CreateArticle.feature"))
+  val csvFeeder = csv("articles.csv").circular()
+
+  val createArticle = scenario("Create and delete article").feed(csvFeeder).exec(karateFeature("classpath:conduitApp/performance/CreateArticle.feature"))
   
   //Inject a user is a session starting our scenario
   setUp(
     createArticle.inject(
       atOnceUsers(1),
       nothingFor(4 seconds),
-      constantUsersPerSec(1) during (10 seconds),
-      constantUsersPerSec(2) during (10 seconds),
-      rampUsersPerSec(2) to 10 during (20 seconds),
-      nothingFor(5 seconds),
       constantUsersPerSec(1) during (5 seconds)
+      // constantUsersPerSec(2) during (10 seconds),
+      // rampUsersPerSec(2) to 10 during (20 seconds),
+      // nothingFor(5 seconds),
+      // constantUsersPerSec(1) during (5 seconds)
     ).protocols(protocol)  
     )
 
